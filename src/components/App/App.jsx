a90c7ebe737ae33d9e7d8b302bbd3e54b8432044
Paweł Konior <pawel.konior@gmail.com>
2022-05-21 14:56:38 +0200

    add navbar component, wrapper for bootstrap link

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,13 +2,18 @@ import React from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 import './App.scss';
+import Header from '../Header/Header';
+import Train from '../Train/Train';
+import Profile from '../Profile/Profile';
 
 function App() {
     return (
         <BrowserRouter>
+            <Header />
             <Routes>
                 <Route path="/" element={<Link to="/profile">Profile</Link>} />
-                <Route path="/profile" element={<Link to="/">Home</Link>} />
+                <Route path="/profile" element={<Profile />} />
+                <Route path="/train" element={<Train />} />
             </Routes>
         </BrowserRouter>
     );
