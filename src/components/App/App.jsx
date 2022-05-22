22976f9036f099fa75f5be6c398c447d461ef0a0
Paweł Konior <pawel.konior@gmail.com>
2022-05-22 09:28:59 +0200

    first implementation of guard

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,19 +1,40 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route, Link, useNavigate } from 'react-router-dom';
 
 import './App.scss';
 import Header from '../Header/Header';
 import Train from '../Train/Train';
 import Profile from '../Profile/Profile';
+import Login from '../Auth/Login';
+import Register from '../Auth/Register';
+import useCookie from '../../hooks/useCookie';
 
 function App() {
+    const [cookie, setCookie] = useCookie('token_auth');
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (cookie === undefined) {
+            navigate('/login');
+        }
+    }, []);
+
     return (
         <BrowserRouter>
             <Header />
             <Routes>
-                <Route path="/" element={<Link to="/profile">Profile</Link>} />
-                <Route path="/profile" element={<Profile />} />
-                <Route path="/train" element={<Train />} />
+                {cookie ? (
+                    <>
+                        <Route path="/" element={<Link to="/profile">Profile</Link>} />
+                        <Route path="/profile" element={<Profile />} />
+                        <Route path="/train" element={<Train />} />
+                    </>
+                ) : (
+                    <>
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/register" element={<Register />} />
+                    </>
+                )}
             </Routes>
         </BrowserRouter>
     );
